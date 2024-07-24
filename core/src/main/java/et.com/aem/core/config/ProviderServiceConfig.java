d157bafc12ffc5a5e1b64ed77659c82a6b5b3e4d
habtamut <>
2024-07-24 16:50:47 +0300

    Update ProviderServiceConfig.java

--- core/src/main/java/et.com/aem/core/config/ProviderServiceConfig.java
+++ core/src/main/java/et.com/aem/core/config/ProviderServiceConfig.java
@@ -6,5 +6,5 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 @ObjectClassDefinition(name = "Provider Service Configuration Url")
 public @interface ProviderServiceConfig {
     @AttributeDefinition(name = "Provider API URL", description = "URL for the Provider API")
-    String apiUrl() default "https://mocki.io/v1/8dc2d896-24a2-46ac-be3b-c37916de8dd8";
+    String apiUrl();
 }
